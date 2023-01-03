1d77b67f3c583e2efc32290264aeedea0817f4e6
nathallyet <nathallyet@gmail.com>
2023-01-02 21:53:45 -0300

    Implementing the Completed Task directive

--- todo-list/src/app/app.component.spec.ts
+++ todo-list/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -11,6 +12,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ]
     }).compileComponents();
   });
 
--- todo-list/src/app/tasks/shared/task-completed.directive.spec.ts
+++ todo-list/src/app/tasks/shared/task-completed.directive.spec.ts
@@ -2,7 +2,7 @@ import { TaskCompletedDirective } from './task-completed.directive';
 
 describe('TaskCompletedDirective', () => {
   it('should create an instance', () => {
-    const directive = new TaskCompletedDirective();
+    const directive = new TaskCompletedDirective(el: ElementRef);
     expect(directive).toBeTruthy();
   });
 });
